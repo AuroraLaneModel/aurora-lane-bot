9bbb786efc2c34530be42404169d30a392e1cfad
AuroraLaneModel <vitorgustavo.2003@hotmail.com>
2025-04-13 19:58:58 -0300

    Delete install.sh

--- install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-apt update && apt upgrade -y
-apt install python3 python3-pip git -y
-git clone https://github.com/SEU-USUARIO/aurora-lane-bot.git
-cd aurora-lane-bot
-pip3 install -r requirements.txt
-echo 'INSTALAÇÃO FINALIZADA. COLOQUE SEU BOT_TOKEN NO .env'
